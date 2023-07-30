3ab50bc3e306df20f6cc4624073f90e488de9031
Ayham Alahmad <86928915+Ayhamalahmad@users.noreply.github.com>
2023-07-30 17:24:31 +0300

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -4,21 +4,16 @@ let counter = container.querySelector(".container span");
 let decreaseBtn = container.querySelector(".container .decrease");
 let resetBtn = container.querySelector(".container .reset");
 let increaseBtn = container.querySelector(".container .increase");
-// Declar This Variable To  Store The Interval
-let intervalId;
 // Add Event To  Increase Button
 increaseBtn.addEventListener("click", () => {
-  clearInterval(intervalId);
-  intervalId = setInterval(increaseCounter, 1000);
+  increaseCounter()
 });
 // Add Event To  Decrease Button
 decreaseBtn.addEventListener("click", () => {
-  clearInterval(intervalId);
-  intervalId = setInterval(decreaseCounter, 1000);
+  decreaseCounter()
 });
 // Add Event To  Reset Button
 resetBtn.addEventListener("click", () => {
-  clearInterval(intervalId);
   counter.textContent = 0;
 });
 //  Increases The Value Displayed In The Counter
